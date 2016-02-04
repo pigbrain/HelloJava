178420ce822069158ef075741e5f113dc15660af
pigbrain <hyundong.lee@nhnent.com>
2016-02-04 11:51:02 +0900

    register HtmlUnit test code

---
+++ src/main/java/htmlunit/HtmlUnit.java
@@ -0,0 +1,40 @@
+package htmlunit;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Assert;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.ScriptResult;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
+public class HtmlUnit {
+
+	public static void main(String[] args) throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+		
+		// This is a test code for search about "google" through google.com
+		
+		try (final WebClient webClient = new WebClient()) {
+			final HtmlPage htmlPage = webClient.getPage("http://www.google.com");
+			Assert.assertEquals("Google", htmlPage.getTitleText());
+
+			Assert.assertTrue(htmlPage.asText().contains("Google+"));
+			
+			final HtmlForm searchForm = htmlPage.getFormByName("f");
+			final HtmlTextInput textField = searchForm.getInputByName("q");
+			textField.setValueAttribute("google");
+
+			final HtmlSubmitInput submitButton = searchForm.getInputByName("btnG");
+			final HtmlPage searchPage = submitButton.click();
+		    
+			Assert.assertTrue(searchPage.asText().contains("Search Options"));
+		}
+
+	}
+}
